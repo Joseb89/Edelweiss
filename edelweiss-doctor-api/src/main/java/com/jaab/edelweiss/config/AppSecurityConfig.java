19ccab8c60ce5fb03097b7af3a98d1f3c1d28ad6
Joseph Barr <jbarr89@yahoo.com>
2024-04-27 08:49:33 -0500

    Updated doctor and prescription APIs

--- edelweiss-doctor-api/src/main/java/com/jaab/edelweiss/config/AppSecurityConfig.java
+++ edelweiss-doctor-api/src/main/java/com/jaab/edelweiss/config/AppSecurityConfig.java
@@ -4,7 +4,6 @@ import com.jaab.edelweiss.model.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -34,7 +33,7 @@ public class AppSecurityConfig {
                         .requestMatchers("/physician/**").hasRole(Role.PHYSICIAN.name())
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-                .httpBasic(Customizer.withDefaults())
+                .httpBasic(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
     }
--- edelweiss-doctor-api/src/main/java/com/jaab/edelweiss/controller/DoctorController.java
+++ edelweiss-doctor-api/src/main/java/com/jaab/edelweiss/controller/DoctorController.java
@@ -9,9 +9,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.*;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -52,8 +52,9 @@ public class DoctorController {
            response.setHeader(HttpHeaders.SET_COOKIE, cookie.toString());
 
            return accessToken;
+
         } else
-            throw new UsernameNotFoundException("Authentication failed.");
+            throw new BadCredentialsException("Invalid email or password");
     }
 
     /**
